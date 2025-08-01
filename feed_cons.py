2ec35a82cad3e227491e008cd4a3b21013f69f7a
themporalyesis <187562925+themporalyesis@users.noreply.github.com>
2025-08-01 18:56:36 +0200

    Update feed_cons.py

--- feed_cons.py
+++ feed_cons.py
@@ -18,7 +18,13 @@ ET.register_namespace('', 'http://www.w3.org/2005/Atom')
 
 
 def fetch_page(url):
-    with urllib.request.urlopen(url) as response:
+    req = urllib.request.Request(
+        url, 
+        headers={
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
+        }
+    )
+    with urllib.request.urlopen(req) as response:
         return response.read().decode("utf-8")
 
 
